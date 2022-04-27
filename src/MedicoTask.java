1f91b0c6b238dc53e8ef35618b964f2b0e235fdf
JavaKhangNguyen <khang2073@gmail.com>
2022-04-27 10:18:12 +0700

    Update MedicoTask.java

--- src/MedicoTask.java
+++ src/MedicoTask.java
@@ -1,4 +1,11 @@
 import org.json.*;
+import java.io.*;
 public class MedicoTask {
-    
+    public static void main(String[] args) {
+    String directory = ""; //JSON directory here?
+    JSONObject obj = new JSONObject(directory);
+    if (!obj.getString("status").equals("OK")){
+        return;
+    }
+    }
 }
