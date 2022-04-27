089d77a5d10ff2031594afd9ea192eccb2f94a50
JavaKhangNguyen <khang2073@gmail.com>
2022-04-27 12:11:28 +0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -12,14 +12,8 @@ public class Person {
     public String getID(){
         return id;
     }
-    public void setID(String id){
-        this.id = id;
-    }
     public String getName(){
         return name;
-    }
-    public void setName(String name){
-        this.name = name;
     }
     public String getEmail(){
         return email;
@@ -36,14 +30,7 @@ public class Person {
     public String getGender() {
         return gender;
     }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
     public String getNationality() {
         return nationality;
     }
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
 }
