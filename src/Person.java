a9df2a7a4b9328f8f06f6b7f7a43907c4bbf9646
JavaKhangNguyen <khang2073@gmail.com>
2022-04-27 11:57:21 +0700

    Update

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,41 @@ public class Person {
         this.gender = gender;
         this.nationality = nationality;
     }
+    public String getID(){
+        return id;
+    }
+    public void setID(String id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
 }
