04947f9f9c831d3c1bfb22156cfd8c49e5e7cde3
JavaKhangNguyen <khang2073@gmail.com>
2022-05-01 09:45:50 +0700

    Update Nurse.java

--- src/Staff/Nurse.java
+++ src/Staff/Nurse.java
@@ -1,5 +1,9 @@
 package Staff;
+import Person.Person;
+public class Nurse extends Person {
+    public Nurse(String id, String name, String email, String phone_number, 
+                  String gender, String nationality) {
+        super(id, name, email, phone_number, gender, nationality);
 
-public class Nurse {
-    
+    }
 }
