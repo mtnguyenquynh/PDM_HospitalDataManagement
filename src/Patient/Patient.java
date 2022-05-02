2bccfa56da95ce92431778f62c6dd4a7ab4a03bd
JavaKhangNguyen <khang2073@gmail.com>
2022-05-02 10:06:45 +0700

    Initialize Patient

--- src/Patient/Patient.java
+++ src/Patient/Patient.java
@@ -1,8 +1,50 @@
 package Patient;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import Person.Person;
-public class Patient extends Person {
+public class Patient extends Person implements Serializable {
+
     public Patient(String id, String name, String email, String phone_number, String gender, String nationality) {
         super(id, name, email, phone_number, gender, nationality);
     }
-    
+
+    JSONParser jsonParser = new JSONParser();
+
+    FileReader reader;
+    // Read JSON file
+    private Object obj;
+    private static JSONArray patient;
+
+    @SuppressWarnings("unchecked")
+
+    public JSONArray PatientData() throws IOException, ParseException {
+        try {
+            reader = new FileReader("C:/Users/WIN 10/git/PDM_HospitalDataManagement-1/data/person/sample_patient.json");
+            obj = jsonParser.parse(reader);
+            patient = (JSONArray) obj;
+
+            System.out.println(patient);
+
+            // Iterate over patient array
+            // patientList.forEach(patient -> parsePatientObject((JSONObject) patient));
+            Iterator<JSONObject> iterator = patient.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return patient;
+    }
+
+    public static int countJsonObject() {
+        return patient.size();
+    }
+
 }
--- src/Patient/PatientManager.java
+++ src/Patient/PatientManager.java
@@ -1,5 +1,61 @@
 package Patient;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Copyright (C) 2022-2022, HDM-Dev Team
+ * All Rights Reserved
+ * This file is part of HDM-Dev Team's project. The contents are
+ * fully covered, controlled, and acknowledged by the terms of the
+ * BSD-3 license, which is included in the file LICENSE.md, found
+ * at the root of the project's source code/tree repository.
+ **/
 public class PatientManager {
-    
+    public void serialized() {
+        ArrayList<Patient> patients = new ArrayList<>();
+        for (int i = 0; i < Patient.countJsonObject(); i++) {
+            // add json object into arraylist
+        }
+        // Serialized
+        try {
+            FileOutputStream fos = new FileOutputStream("PatientData");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(patients);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public void deserialized() {
+        // Deserialized
+        ArrayList<Patient> patients = new ArrayList<>();
+
+        try {
+            FileInputStream fis = new FileInputStream("PatientData");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            patients = (ArrayList) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
+
+        // Verify list data
+        for (Patient patient : patients) {
+            System.out.println(patient);
+        }
+    }
 }
